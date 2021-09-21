b56034be6c42c06c1c4d70d6fe3edddab68531b9
Iuri Néviton <iurineviton@gmail.com>
2021-09-21 17:25:00 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,5 +1,8 @@
 package one.digitalinnovation.personapi.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +29,11 @@ public class PersonService { // regras de negocio
 
 		Person savedPerson = personRepository.save(personToSave);
 		return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
+	}
+
+	public List<PersonDTO> listAll() {
+		List<Person> allPeople = personRepository.findAll();
+		return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
 	}
 
 }
